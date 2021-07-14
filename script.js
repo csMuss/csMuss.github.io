caa7f303d794f64489b15d2a0c4b05c48159a599
Nick <chesinick@gmail.com>
2021-07-14 14:19:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 var button = document.getElementById("btn");
+var inputWindowOne = prompt("Please enter varible 1 and 2","1:","2:");
+var input1;
+var input2;
+var output;
 
 function click(){
     document.getElementById("dateBox").innerHTML = Date();
+}
 
+function add(){
+    output = input1 + input2;
+    return output;
 }
 
 
+
+ 
